b05269b56cf3c7337554e7227e0ac6e016b9afbd
DK-Shanto <shanto1709@cseku.ac.bd>
2021-11-08 18:45:41 +0600

    cart updated

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,21 +1,33 @@
 import React from 'react';
 
 const Cart = (props) => {
-    let total = 0;
-    let quantity = 0;
+
     const { cart } = props;
+
+    // const totalReducer = (previous, product) => previous + product.price;
+    // const total = cart.reduce(totalReducer, 0);
+    let totalQuantity = 0;
+    let total = 0;
     for (const product of cart) {
         if (!product.quantity) {
             product.quantity = 1;
         }
         total = total + product.price * product.quantity;
-        quantity = quantity + product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
     }
+
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
-            <h2>Order Summary</h2>
-            <h5>Product Order: {quantity}</h5>
-            <h3>Total price: {total}</h3>
+            <h3>Order Summary</h3>
+            <h5>Items Ordered: {totalQuantity}</h5>
+            <br />
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
